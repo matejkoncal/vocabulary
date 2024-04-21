15e85d97acacc2398612046219c60ac05f6e6753
Matej Koncal <matej.koncal@resco.net>
2024-04-21 20:33:48 +0200

    first card implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,25 @@
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.scss";
+import CssBaseline from "@mui/material/CssBaseline";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { Card } from "./card/card";
+
+const theme = createTheme({
+  palette: {
+    mode: getSystemTheme(),
+  },
+});
 
 function App() {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Card top="Top" bottom="Bottom" />
+    </ThemeProvider>
+  );
 }
 
-export default App
+function getSystemTheme() {
+  return window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light";
+}
+
+export default App;
---
+++ src/card/card.tsx
@@ -0,0 +1,30 @@
+import { Box, Container, Paper } from "@mui/material"
+import { useState } from "react"
+import ReactCardFlip from "react-card-flip"
+
+interface CardProps {
+  top: string,
+  bottom: string
+}
+
+export const Card = ({ top, bottom }: CardProps) => {
+  const [isFlipped, setIsFlipped] = useState(false)
+
+  return (
+    <Container style={{ maxWidth: "600px" }}>
+      <ReactCardFlip isFlipped={isFlipped} flipDirection="vertical">
+        <Paper elevation={3} onClick={() => setIsFlipped(!isFlipped)}>
+          <Box justifyContent="center" alignItems="center" display="flex" height="300px">
+            {top}
+          </Box>
+        </Paper>
+        <Paper onClick={() => setIsFlipped(!isFlipped)}>
+          <Box justifyContent="center" alignItems="center" display="flex" height="300px">
+            {bottom}
+          </Box>
+        </Paper>
+      </ReactCardFlip>
+    </Container>
+
+  )
+}
